74452d2b97b0441f4d43fd26e34082d0479de5fa
pedroaurelli <pedroaureliano.contato@gmail.com>
2022-01-27 22:21:25 -0300

    change bg

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -40,7 +40,6 @@ export default function ChatPage() {
             styleSheet={{
                 display: 'flex', alignItems: 'center', justifyContent: 'center',
                 backgroundColor: appConfig.theme.colors.primary[500],
-                backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
                 backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 color: appConfig.theme.colors.neutrals['000']
             }}
@@ -129,6 +128,9 @@ export default function ChatPage() {
                                 color: appConfig.theme.colors.neutrals[200],
                             }}
                         />
+
+                            {/* LUGAR ONDE VAI FICAR O MEU BTN DE ENVIAR */}
+
                     </Box>
                 </Box>
             </Box>
